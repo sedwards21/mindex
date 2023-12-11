0cf3c6b14e8eeeb21173d2a8a60ea03f0a05a76b
Shane Edwards <edwardss212@gmail.com>
2023-12-11 09:33:33 -0500

    Update ReportingStructureServiceImpl.java

--- src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
@@ -4,6 +4,8 @@ import com.mindex.challenge.data.Employee;
 import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.EmployeeService;
 import com.mindex.challenge.service.ReportingStructureService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,8 @@ import java.util.List;
 
 @Service
 public class ReportingStructureServiceImpl implements ReportingStructureService {
+    private static final Logger LOG = LoggerFactory.getLogger(EmployeeServiceImpl.class);
+
     @Autowired
     private final EmployeeService employeeService;
 
@@ -23,6 +27,8 @@ public class ReportingStructureServiceImpl implements ReportingStructureService 
     }
 
     public int getEmployeeReportCount(Employee employee) {
+        LOG.debug("Getting report count for employee [{}]", employee);
+
         List<Employee> directReports = employee.getDirectReports();
         int result = directReports.size();
         for (Employee emp : directReports) {
